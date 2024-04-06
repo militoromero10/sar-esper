f71711ab8d53fb593ac5547a6612617f05373171
milinromero <milito@romero.com>
2024-04-06 09:52:51 -0500

    esper base

---
+++ src/main/java/com/javeriana/esper/EsperApplication.java
@@ -0,0 +1,14 @@
+package com.javeriana.esper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsperApplication.class, args);
+	}
+
+	////https://esper.espertech.com/release-7.1.0/esper-reference/html/gettingstarted.html
+}
---
+++ src/test/java/com/javeriana/esper/EsperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javeriana.esper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
